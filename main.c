2578dd23cea4a23521f7cfac259cb9a970c5d031
Gabriel-ADM <gabriel.admpinho@gmail.com>
2025-11-26 12:07:04 -0300

    feat(sensor) :construction: Criacao de struct para sensor de incendio e funcao para imprimir sensor

--- main.c
+++ main.c
@@ -4,28 +4,61 @@
 #define SMALL_GRID 3
 #define WHOLE_GRID 30
 
-void printGreenBg(char *text)
+void printCharGreenBg(char character)
 {
-    printf("\033[42m %s \033[m", text);
+    printf("\033[42m %c \033[m", character);
 }
 
-int main(int argc, char const *argv[])
+typedef struct Sensor
+{
+    int id;
+    char matrix[SMALL_GRID][SMALL_GRID];
+    int posX, posY;
+} Sensor;
+
+Sensor *initiateSensor(int id, int positionX, int positionY)
+{
+    Sensor *sensor = malloc(sizeof(Sensor));
+    if (sensor == NULL) {
+        fprintf(stderr, "Memory allocation of sensor id: %d failed\n", id);
+        return NULL;
+    }
+    sensor->id = id;
+    sensor->posX = positionX;
+    sensor->posY = positionY;
+    for (int i = 0; i < SMALL_GRID; i++)
+    {
+        for (int j = 0; j < SMALL_GRID; j++)
+        {
+            if (i == 1 && j == 1)
+            {
+                sensor->matrix[i][j] = 'T';
+            }
+            else
+            {
+                sensor->matrix[i][j] = '-';
+            }
+        }
+    }
+    return sensor;
+}
+
+void printSensor(Sensor *sensor)
 {
     for (int i = 0; i < SMALL_GRID; i++)
     {
         for (int j = 0; j < SMALL_GRID; j++)
         {
-            if (i == SMALL_GRID / 2 && j == SMALL_GRID / 2)
-            {
-                printGreenBg("T");
-            }
-            else
-            {
-                printGreenBg("-");
-            }
+            printCharGreenBg(sensor->matrix[i][j]);
         }
         printf("\n");
     }
+}
+
+int main(int argc, char const *argv[])
+{
+    Sensor* first = initiateSensor(1,1,1);    
+    printSensor(first);
 
     return 0;
 }
