e3e83cc597b075584e6e01628bb8236392d7d60f
Gabriel-ADM <gabriel.admpinho@gmail.com>
2025-11-27 16:42:41 -0300

    feat(sensor) :construction: Criacao de funncao para impressao de grid/grade de sensores

--- main.c
+++ main.c
@@ -19,7 +19,8 @@ typedef struct Sensor
 Sensor *initiateSensor(int id, int positionX, int positionY)
 {
     Sensor *sensor = malloc(sizeof(Sensor));
-    if (sensor == NULL) {
+    if (sensor == NULL)
+    {
         fprintf(stderr, "Memory allocation of sensor id: %d failed\n", id);
         return NULL;
     }
@@ -55,10 +56,63 @@ void printSensor(Sensor *sensor)
     }
 }
 
+Sensor initiateGrid(Sensor *grid[WHOLE_GRID][WHOLE_GRID])
+{
+    int id = 0;
+    for (size_t i = 0; i < WHOLE_GRID; i++)
+    {
+        for (size_t j = 0; j < WHOLE_GRID; j++)
+        {
+            id++;
+            grid[i][j] = initiateSensor(id, i, j);
+        }
+    }
+}
+
+void freeGrid(Sensor *grid[WHOLE_GRID][WHOLE_GRID])
+{
+    for (size_t i = 0; i < WHOLE_GRID; i++)
+    {
+        for (size_t j = 0; j < WHOLE_GRID; j++)
+        {
+            free(grid[i][j]);
+        }
+    }
+}
+
+void printSensorGrid(Sensor *grid[WHOLE_GRID][WHOLE_GRID])
+{
+    size_t iIdx = 0, jIdx = 0;
+    // printing horizontal coordinates to better visualization
+    printf("%5s", ""); // 5 was chosen because the rows are formatted as %4ld and the first is always zero
+    for (jIdx; jIdx < SMALL_GRID * WHOLE_GRID; jIdx++)
+        printf("%2ld ", jIdx);
+    printf("\n");
+
+    for (size_t j = 0; j < WHOLE_GRID; j++)
+    {
+        for (size_t jSensor = 0; jSensor < SMALL_GRID; jSensor++)
+        {
+            for (size_t i = 0; i < WHOLE_GRID; i++)
+            {
+                // printing vertical coordinates to better visualization
+                if (i == 0)
+                    printf("%4ld ", iIdx++);
+                for (size_t iSensor = 0; iSensor < SMALL_GRID; iSensor++)
+                    printCharGreenBg(grid[i][j]->matrix[iSensor][jSensor]);
+            }
+            printf("\n");
+        }
+    }
+}
+
 int main(int argc, char const *argv[])
 {
-    Sensor* first = initiateSensor(1,1,1);    
-    printSensor(first);
+    Sensor *sensors[WHOLE_GRID][WHOLE_GRID];
+    initiateGrid(sensors);
 
+    printSensorGrid(sensors);
+
+    freeGrid(sensors);
     return 0;
 }
